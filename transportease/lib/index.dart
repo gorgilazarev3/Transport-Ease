2647f5b472e71954beac226e1fae7b5243fc3722
gorgilazarev3 <gorgi.lazarev3@gmail.com>
2023-08-21 17:37:07 +0200

    Created a few screens and added FlutterFlow library

---
+++ transportease/lib/index.dart
@@ -0,0 +1,3 @@
+// Export pages
+export 'package:transportease/Screens/landing_page.dart' show LandingPage;
+export 'package:transportease/Screens/login_page.dart' show LoginPageWidget;
--- transportease/lib/main.dart
+++ transportease/lib/main.dart
@@ -1,20 +1,93 @@
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_localizations/flutter_localizations.dart';
+import 'package:transportease/Screens/login_page.dart';
 
-void main() {
+import 'Screens/landing_page.dart';
+import 'flutter_flow/flutter_flow_theme.dart';
+import 'flutter_flow/internationalization.dart';
+import 'flutter_flow/nav/nav.dart';
+
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  if (kIsWeb) {
+    await Firebase.initializeApp(
+        options: const FirebaseOptions(
+            apiKey: "AIzaSyDJDtIjXxPSPDfRwnKSO2gGvS3s7gdLAfs",
+            appId: "1:376309427810:web:48af3118670cb068f02b3b",
+            messagingSenderId: "376309427810",
+            projectId: "transportease-da2b7"));
+  }
+  await Firebase.initializeApp();
+
   runApp(const TransportEaseApp());
 }
 
-class TransportEaseApp extends StatelessWidget {
-  const TransportEaseApp({super.key});
+class _MyAppState extends State<TransportEaseApp> {
+  Locale? _locale;
+  ThemeMode _themeMode = FlutterFlowTheme.themeMode;
 
-  // This widget is the root of your application.
+  late AppStateNotifier _appStateNotifier;
+  late GoRouter _router;
+
+  @override
+  void initState() {
+    super.initState();
+    _appStateNotifier = AppStateNotifier.instance;
+    _router = createRouter(_appStateNotifier);
+  }
+
+  void setLocale(String language) {
+    setState(() => _locale = createLocale(language));
+  }
+
+  void setThemeMode(ThemeMode mode) => setState(() {
+        _themeMode = mode;
+        FlutterFlowTheme.saveThemeMode(mode);
+      });
+
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      title: "TransportEase",
-      home: Text("TransportEase App"),
+    return MaterialApp.router(
+      title: 'TransportEase',
+      localizationsDelegates: [
+        FFLocalizationsDelegate(),
+        GlobalMaterialLocalizations.delegate,
+        GlobalWidgetsLocalizations.delegate,
+        GlobalCupertinoLocalizations.delegate,
+      ],
+      locale: _locale,
+      supportedLocales: const [Locale('en', '')],
+      theme: ThemeData(
+        brightness: Brightness.light,
+        scrollbarTheme: ScrollbarThemeData(),
+      ),
+      darkTheme: ThemeData(
+        brightness: Brightness.dark,
+        scrollbarTheme: ScrollbarThemeData(),
+      ),
+      themeMode: _themeMode,
+      routerConfig: _router,
     );
   }
 }
 
+class TransportEaseApp extends StatefulWidget {
+  const TransportEaseApp({super.key});
 
+  @override
+  State<TransportEaseApp> createState() => _MyAppState();
+
+  static _MyAppState of(BuildContext context) =>
+      context.findAncestorStateOfType<_MyAppState>()!;
+
+  // This widget is the root of your application.
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: "TransportEase",
+      home: const LandingPage(),
+      theme: ThemeData(fontFamily: "Nunito"),
+    );
+  }
+}

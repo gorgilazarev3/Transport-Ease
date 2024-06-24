6613be57230663f920ed2103edb12af77491e446
gorgilazarev3 <gorgi.lazarev3@gmail.com>
2024-06-25 00:07:53 +0200

    Added Dockerfile and nginx files for deployment

---
+++ src_backend/TransportEase_backend/scripts/00-laravel-deploy.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env bash
+echo "Running composer"
+composer global require hirak/prestissimo
+composer install --no-dev --working-dir=/var/www/html
+
+echo "generating application key..."
+php artisan key:generate --show
+
+echo "Caching config..."
+php artisan config:cache
+
+echo "Caching routes..."
+php artisan route:cache
+
+echo "Running migrations..."
+php artisan migrate --force
+
+echo "Seeding database..."
+php artisan db:seed
